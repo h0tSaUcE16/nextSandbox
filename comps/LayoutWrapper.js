cf10ea4786a6dd6a3eaa4daa8520372f48783483
Joseph Michael Corrado <joeyycorrss@gmail.com>
2020-01-08 15:29:32 -0500

    Added Head changes (title, icon)

--- comps/LayoutWrapper.js
+++ comps/LayoutWrapper.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from './Header'
 
 const layoutStyle = {
@@ -9,6 +10,18 @@ const layoutStyle = {
 export default function Layout(props) {
   return (
     <div style={layoutStyle}>
+      <Head>
+        <title>Bat Page</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <link
+          rel="icon"
+          href="https://c7.uihere.com/files/711/952/883/batman-joker-logo-symbol-knight-vector.jpg"
+        />
+      </Head>
       <Header />
       {props.children}
     </div>
